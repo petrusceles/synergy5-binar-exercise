9ca3eb4bf704fabd768640dd0de5fe4ca469ea60
Raihan Rafid <raihanrfd1@gmail.com>
2022-09-30 22:05:37 +0700

    materials/chapter-3-intro-js

---
+++ chapter3/introjs/js/script.js
@@ -0,0 +1,103 @@
+// console.log("Halo JS external");
+// console.log("Halo JS 2");
+
+// var studentName = "Munawar";
+// studentName = false;
+
+// console.log(studentName);
+
+// Scope
+// var diskon = 500;
+
+// if (true) {
+//   var diskon = 300;
+//   console.log(diskon)
+// }
+
+// console.log(diskon)
+
+// Reassigned dan Redeclared
+// Assign
+// const studentName = "Fachrul";
+// const genderMale = "male"
+// const genderFemale = "female"
+
+// console.log(studentName)
+
+// studentName = "Rama"
+
+// console.log(studentName)
+
+// // create data user
+// // id, username, password
+// const id = username + password
+
+// // Ketika simpen ke database
+// saveToDB(id, username, password)
+
+
+// var studentName = "Rama"
+// console.log(studentName)
+
+// Array
+// const studentNames = ["evi", "wahyu", "maftuh", { name: "munawar", childs: [1, 2, 3] }];
+
+// // gabisa, disini doang constannya
+// studentNames = ["asd"]
+
+// studentNames[1] = "dikky dan dita"
+
+// console.log(studentNames)
+// console.log(studentNames[3].childs)
+
+// Object
+// const person = {
+//   age: 22, // attribute
+//   isHandsome: true,
+//   isMarried: false,
+//   0: "nol",
+//   childs: [
+//     "rayhan", "bunga", "hana"
+//   ],
+//   // wife: {
+//   //   name: "aldi",
+//   //   age: 70,
+//   //   pet: {
+//   //     name: "bunga"
+//   //   }
+//   // }
+// }
+
+// console.log(person.wife.pet.name)
+
+// Latihan JS
+const steamAccount = {
+  email: "rafid@gmail.com",
+  password: "rafid123",
+  isVerified: true,
+  wallet: 5000000,
+  games: [
+    {
+      name: "DOTA 2",
+      rank: "Guardian"
+    },
+    {
+      name: "Valorant",
+      rank: "Iron 3"
+    }
+  ],
+  friends: [
+    {
+      email: "raihan@gmail.com",
+      password: "raihan",
+      isVerified: true,
+      wallet: 2000000,
+      games: [
+        {
+          name: "DOTA 2",
+          rank: "Guardian"
+        },
+      ],
+    }
+  ]
+}
